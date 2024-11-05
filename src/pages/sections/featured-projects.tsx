bcaff9f5f6f78ef596ff49bd4218c2e0430583d4
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-11-05 12:14:07 +0400

    Update featured-projects.tsx

--- src/pages/sections/featured-projects.tsx
+++ src/pages/sections/featured-projects.tsx
@@ -61,9 +61,9 @@ export default function FeaturedProjects() {
             }}
           >
             <Project
-              img="/view_1.png"
-              title="MAK Sohar Chemical Industries"
-              desc="Complete Construction of Warehouse facility for MAK SOHAR"
+              img="https://static6.depositphotos.com/1000292/649/i/450/depositphotos_6490033-stock-photo-water-recycling-on-sewage-treatment.jpg"
+              title="Project 1"
+              desc="Brief description of project 1"
             />
 
             <Project
